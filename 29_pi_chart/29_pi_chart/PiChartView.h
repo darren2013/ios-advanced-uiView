5dcd7c70095d3492c076866194eb889f6c5a419d
darren.du <ddf168913@qq.com>
2015-12-10 15:57:07 +0800

    draw pi chart with quartz2d

---
+++ 29_pi_chart/29_pi_chart/PiChartView.h
@@ -0,0 +1,13 @@
+//
+//  PiChartView.h
+//  29_pi_chart
+//
+//  Created by 杜东方 on 15/12/10.
+//  Copyright © 2015年 private. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PiChartView : UIView
+
+@end
---
+++ 29_pi_chart/29_pi_chart/PiChartView.m
@@ -0,0 +1,21 @@
+//
+//  PiChartView.m
+//  29_pi_chart
+//
+//  Created by 杜东方 on 15/12/10.
+//  Copyright © 2015年 private. All rights reserved.
+//
+
+#import "PiChartView.h"
+
+@implementation PiChartView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
